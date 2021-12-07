87087977d2cfde83d5b2430eb9b6526a7ce6a213
Sumedh Nikhil Shah <59042979+Sumedh1197@users.noreply.github.com>
2021-12-07 18:05:07 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -7,8 +7,8 @@ from PyQt5.QtWidgets import *
 from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from darktheme.widget_template import DarkPalette
-import qdarktheme
+#from darktheme.widget_template import DarkPalette
+#import qdarktheme
 
 from clustering import *
 from weather import *
